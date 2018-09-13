ef71013744e708f53d5c55e84007a0f05cba32ad
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-13 22:30:27 +0800

    开发业务组件B需求

--- ModuleB-Component/Controller/ModuleBViewController.m
+++ ModuleB-Component/Controller/ModuleBViewController.m
@@ -1,25 +1,25 @@
 //
-//  ViewController.m
+//  ModuleBViewController.m
 //  ModuleB-Component
 //
 //  Created by 刘光强 on 2018/9/13.
 //  Copyright © 2018年 quangqiang. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "ModuleBViewController.h"
 #import "PageBViewController.h"
 
-@interface ViewController ()
+@interface ModuleBViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ModuleBViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
+    // Do any additional setup after loading the view.
     self.title = @"我是模块B业务组件";
+    
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(0, 0, 300, 100);
     btn.backgroundColor = [UIColor greenColor];
@@ -33,10 +33,20 @@
     PageBViewController *VC = [[PageBViewController alloc] init];
     [self.navigationController pushViewController:VC animated:YES];
 }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
